5366b8799118d4e265896fd45e94566569cc79ca
Mohan Satvik <adusumallimohansatvik@gmail.com>
2026-07-30 21:41:04 +0530

    added harry potter knowledge

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from rag import rag_init, rag_query
 # load_dotenv()
 
 # FILE_PATH = os.getenv("RAG_FILE_PATH")
-FILE_PATH = "knowledge.txt"
+FILE_PATH = "harry_potter_knowledge_base.txt"
 vector_store = rag_init(FILE_PATH)
 
 def chat(message, history):
